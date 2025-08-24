7482da41b86853847ead1eb3bd78a52203f50685
Scott Shih <1365809+ScottShih1996@users.noreply.github.com>
2025-08-25 03:28:24 +0800

    Update PostHeader.js

--- themes/commerce/components/PostHeader.js
+++ themes/commerce/components/PostHeader.js
@@ -34,7 +34,7 @@ export default function PostHeader({ post }) {
             {siteConfig('POST_TITLE_ICON') && <NotionIcon icon={post.pageIcon} className='text-4xl mx-1' />}<div className='text-4xl mx-1'>{post.title}</div>           
           </div>
    
-          <div className='justify-center text-white'>       
+          <div className='justify-center text-white margin-inline'>       
           {ANALYTICS_BUSUANZI_ENABLE && (
               <div className='hidden busuanzi_container_page_pv'>
                 <i className='fas fa-eye mr-1' />
