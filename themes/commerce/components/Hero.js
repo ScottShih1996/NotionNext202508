10421957e39175bd42b3807ac4fb9ecf9d708017
Scott Shih <1365809+ScottShih1996@users.noreply.github.com>
2025-09-11 20:59:42 +0800

    Update Hero.js

--- themes/commerce/components/Hero.js
+++ themes/commerce/components/Hero.js
@@ -10,7 +10,7 @@ const Hero = props => {
   const { siteInfo } = props
 
   return (
-        <header id="header" className="w-full h-96 relative md:flex-shrink-0 z-10" >
+        <header id="header" className="w-full h-72 relative md:flex-shrink-0 z-10" >
 
             <div className="text-white absolute bottom-0 flex flex-col h-full items-center justify-center w-full "></div>
 
