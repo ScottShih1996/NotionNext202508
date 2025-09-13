9481b3a0f45b611d4986a6d72d51d55757faf38c
Scott Shih <1365809+ScottShih1996@users.noreply.github.com>
2025-09-13 12:26:49 +0800

    Update index.js

--- themes/commerce/index.js
+++ themes/commerce/index.js
@@ -141,7 +141,7 @@ const LayoutIndex = props => {
       {latestPosts && latestPosts.length > 0 && (
         <div className='bg-white border-[#D2232A] p-4 mt-2 mb-4'>
           <div className='notion-callout-text text-lg font-bold border-b-2 py-2 border-[#D2232A]'> 
-            <i className='fas fa-history'/>
+            <i className='fas fa-history mr-1'/>
                 {locale.COMMON.RECENT_POSTS}
           </div>
           {siteConfig('POST_LIST_STYLE') === 'page' ? (
