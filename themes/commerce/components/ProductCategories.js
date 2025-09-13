446cef06343bda4ebc568e0bd43f3f73d7af1038
Scott Shih <1365809+ScottShih1996@users.noreply.github.com>
2025-09-13 14:34:43 +0800

    Update ProductCategories.js

--- themes/commerce/components/ProductCategories.js
+++ themes/commerce/components/ProductCategories.js
@@ -16,6 +16,7 @@ export default function ProductCategories(props) {
             CONFIG
           )}
         </div>
+        <SearchButton {...props} />
         <nav
           id='home-nav-button'
           className={'flex flex-col space-y-2 text-start'}>
