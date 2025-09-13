d386d23224d8bf759f9940918edf567801dae54c
Scott Shih <1365809+ScottShih1996@users.noreply.github.com>
2025-09-13 13:39:56 +0800

    Create SearchButton.js

---
+++ themes/commerce/components/SearchButton.js
@@ -0,0 +1,39 @@
+import { siteConfig } from '@/lib/config'
+import { useGlobal } from '@/lib/global'
+import dynamic from 'next/dynamic'
+import { useRouter } from 'next/router'
+import { useRef } from 'react'
+
+const AlgoliaSearchModal = dynamic(() => import('@/components/AlgoliaSearchModal'), { ssr: false })
+
+/**
+ * 搜索按钮
+ * @returns
+ */
+export default function SearchButton(props) {
+  const { locale } = useGlobal()
+  const router = useRouter()
+  const searchModal = useRef(null)
+
+  function handleSearch() {
+    if (siteConfig('ALGOLIA_APP_ID')) {
+      searchModal.current.openSearch()
+    } else {
+      router.push('/search')
+    }
+  }
+
+  return (
+    <>
+      <div
+        onClick={handleSearch}
+        title={locale.NAV.SEARCH}
+        alt={locale.NAV.SEARCH}
+        className='flex justify-center items-center w-7 h-7 cursor-pointer transform hover:scale-105 duration-200 text-gray-500 dark:text-gray-200'
+      >
+        <i className='fa-solid fa-magnifying-glass' />
+      </div>
+      <AlgoliaSearchModal cRef={searchModal} {...props} />
+    </>
+  )
+}
